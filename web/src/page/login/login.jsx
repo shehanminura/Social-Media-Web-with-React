61ba8a1298cce1ba6e52acb5f628d8b67b70a468
shehanminura <shehanminuradp@gmail.com>
2024-06-06 15:46:14 +0530

    create login page

--- web/src/page/login/login.jsx
+++ web/src/page/login/login.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import './login.css';
 
 export default function Login() {
   return (
-
-
-    <div>login</div>
+    <div className="Login">
+      <div className='card'>
+        <div className="left">
+          <h2>NapaExtra<br/>-</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+            nostrum, vitae eum ipsam libero delectus optio!
+          </p>
+          <span>Don't Have An Account?</span>
+          <button className='btn btn-primary'>Register</button>
+        </div>
+        <form className="right">
+          <input type='text' required placeholder='Username'/>
+          <input type='password' required placeholder='Password'/>
+          <button type='submit' className='btn'>Login</button>
+        </form>
+      </div>
+    </div>
   );
 }
