b866aa61de5df2afb4d26d7acf99ac84d2c87c42
shehanminura <shehanminuradp@gmail.com>
2024-06-06 18:04:08 +0530

    Page Routing

--- web/src/LayOut/layOut.jsx
+++ web/src/LayOut/layOut.jsx
@@ -1,10 +1,50 @@
-import {RouterProvider,createBrowserRouter} from 'react-router-dom'
+import {Outlet, RouterProvider,createBrowserRouter} from 'react-router-dom'
 
 import Login from '../page/login/Login';
 import Signup from '../page/signup/Signup'; // Correct import
+import Home from '../page/home/Home';
+import Profile from '../page/profile/Profile';
+import ChatBox from '../page/Chatbox/ChatBox';
 
 export default function Layout() {
+
+//feed................
+
+const Feed =()=>{
+  return(
+    <>
+    <main>
+      <div className="contaner">
+        <Outlet />
+      </div>
+    </main>
+
+    </>
+  )
+}
+
+
+// Router..........................
+
   const router = createBrowserRouter([
+    {
+      path:'/',
+      element: <Feed/> ,
+      children:[
+        {
+          path:'/',
+          element:<Home/>
+        },
+        {
+          path:'/profile/:id',
+          element:<Profile/>
+        },
+        {
+          path:'/chatbox/:id',
+          element:<ChatBox/>
+        },
+      ]
+    },
     {
       path:'/login',
       element:<Login/>
