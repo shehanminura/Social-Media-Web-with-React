1cd8a04a421310e08d8c4b806095c9e7ca002d99
shehanminura <shehanminuradp@gmail.com>
2024-06-09 16:21:08 +0530

    projects all finished

--- web/src/components/feeds/Feeds.jsx
+++ web/src/components/feeds/Feeds.jsx
@@ -1,18 +1,13 @@
 import './feeds.css';
-
-// Components
 import Feed from './feed';
-
-// Fake Api
-import HomeFeedData from '../../FackApis/FirendReqData';
+import HomeFeedData from '../../FackApis/HomeFeedData';  // Ensure this path is correct
 
 export default function Feeds() {
   return (
     <div className='feeds'>
       {HomeFeedData.map(fed => (
-        <Feed fed={fed} key={fed.key} />
+        <Feed fed={fed} key={fed.id} />
       ))}
     </div>
   );
 }
-
--- web/src/components/feeds/feed.jsx
+++ web/src/components/feeds/feed.jsx
@@ -1,58 +1,50 @@
-import './feeds.css'
-import { Link } from 'react-router-dom'
+import './feeds.css';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faComment, faHeart, faListDots, faShare } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
+import Comments from '../comments/Comments';
 
-//Components .................... 
+export default function Feed({ fed }) {
+  const [openComment, setOpenComment] = useState(false);
 
-
-//FontAwesome Icon ....................
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faComment, faHeart, faListDots, faShare } from '@fortawesome/free-solid-svg-icons'
-
-
-// States....................
-import { useState } from 'react'
-import Comments from '../comments/Comments'
-
-export default function feed(fed) {
-
-    //states Discuse......................
-
-    let [openComment,setOpenComment] = useState(false)
-    const CommentHandeler = ()=>{
-        setOpenComment(!openComment)
-    }
+  const CommentHandeler = () => {
+    setOpenComment(!openComment);
+  }
 
   return (
     <div className='feed'>
-        <div className='top-content'>
-            <Link to='/profile/id'>
-            <div className='user'>
-                <img src={fed.feedProfile} alt=''/>
-                <div>
-                <h5>{fed.name}</h5>
-                <small>1 Minutes Ago</small>
+      <div className='top-content'>
+        <Link to={`/profile/${fed.userid}`}>
+          <div className='user'>
+            <img src={fed.feedProfile} alt='' />
+            <div>
+              <h5>{fed.name}</h5>
+              <small>1 Minute Ago</small>
             </div>
-            </div>
-            </Link>
-            <span><FontAwesomeIcon icon={faListDots}/></span>
-        </div>
+          </div>
+        </Link>
+        <span><FontAwesomeIcon icon={faListDots} /></span>
+      </div>
 
-        <div className='mid-content'>
-            <p>{fed.desc}</p>
-            <img src={fed.feedImage} alt=''/>
+      <div className='mid-content'>
+        <p>{fed.desc}</p>
+        <img src={fed.feedImage} alt='' />
+      </div>
+
+      <div className='bottom-content'>
+        <div className='action-item'>
+          <span><FontAwesomeIcon icon={faHeart} />14 Likes</span>
         </div>
-        <div className='bottom-content'>
-            <div className='action-item'>
-                <span><FontAwesomeIcon icon={faHeart}/>14 Like</span>
-            </div>
-            <div className='action-item' onClick={CommentHandeler}>
-                <span><FontAwesomeIcon icon={faComment}/>2 Like</span>
-            </div>
-            <div className='action-item'>
-                <span><FontAwesomeIcon icon={faShare}/>1 Like</span>
-            </div>
+        <div className='action-item' onClick={CommentHandeler}>
+          <span><FontAwesomeIcon icon={faComment} />2 Comments</span>
         </div>
-        {openComment && <Comments/>}
+        <div className='action-item'>
+          <span><FontAwesomeIcon icon={faShare} />1 Share</span>
+        </div>
+      </div>
+
+      {openComment && <Comments />}
     </div>
-  )
+  );
 }
